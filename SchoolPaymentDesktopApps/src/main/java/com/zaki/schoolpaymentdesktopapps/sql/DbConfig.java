16ca961917a1e017516906f5657cfd74a6a980e7
Soeltan Zaki Rizaldy <soeltanzaki.rizaldy@photoninfotech.net>
2018-06-06 17:34:16 +0700

    add conection file

--- SchoolPaymentDesktopApps/src/main/java/com/zaki/schoolpaymentdesktopapps/sql/DbConfig.java
+++ SchoolPaymentDesktopApps/src/main/java/com/zaki/schoolpaymentdesktopapps/sql/DbConfig.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package com.zaki.schoolpaymentdesktopapps.sql;
-
 /**
  *
  * @author soeltanzaki_r
--- SchoolPaymentDesktopApps/src/main/java/com/zaki/schoolpaymentdesktopapps/sql/DbConnection.java
+++ SchoolPaymentDesktopApps/src/main/java/com/zaki/schoolpaymentdesktopapps/sql/DbConnection.java
@@ -10,17 +10,24 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import com.zaki.schoolpaymentdesktopapps.sql.DbConfig;
 /**
  *
  * @author soeltanzaki_r
  */
-public class DbConnection {
+public class DbConnection extends DbConfig{
     
-static Connection connection = null;
-static PreparedStatement statment = null;
-    
-    private static void makeJDBCConnection() {
-    
+    private Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(getSqlHost());
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DbConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return connection;
     }
     
 }
